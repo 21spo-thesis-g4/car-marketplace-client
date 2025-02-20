d83fd45c95167ad956a3930d72abe86168b68e44
Antti Kurkinen <antti.kurkinen01@gmail.com>
2025-02-20 17:07:27 +0200

    ClientConfig

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import Navbar from "./components/navbar";
+import ClientConfig from "./components/clientConfig";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
+         <ClientConfig />
         {children}
       </body>
     </html>
