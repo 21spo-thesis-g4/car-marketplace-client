75966b6923424771dc85b5d27e0e4ef4ab939480
Antti Kurkinen <antti.kurkinen01@gmail.com>
2025-02-20 17:13:14 +0200

    Update env

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig = {
     config.devtool = false; // Disables source maps in development
     return config;
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
